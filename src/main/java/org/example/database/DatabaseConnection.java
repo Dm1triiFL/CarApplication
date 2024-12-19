4b25afd178ae53cc9ae635630211a6cab7e36716
bunggmoon <135305628+Dm1triiFL@users.noreply.github.com>
2024-12-19 21:51:30 +0800

    db

--- src/main/java/org/example/database/DatabaseConnection.java
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:h2:~/test";
+    private static final String URL = "jdbc:h2:tcp://localhost/~/test";
     private static final String USER = "sa"; //
-    private static final String PASSWORD = "pass";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
